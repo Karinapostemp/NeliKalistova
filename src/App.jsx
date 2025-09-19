636977bcdf444e6f84e83edb67ea83c5499ed37d
fikretellek <fikretellek@icloud.com>
2025-09-19 21:14:03 +0100

    I love Karina <3

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,63 @@
-import { useState } from 'react'
-import tigerPicture from './assets/tiger-min.jpg'
-import './App.css'
+import tigerPicture from "./assets/tiger-min.jpg";
+import sobreMi from "./assets/sobre-mi.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <header id='header-id'>
-        <a className='tab1 general-tab' href=''>Pinturas</a>
-        <a className='tab2 general-tab' href=''>Murales</a>
-        <a className='logo general-tab' href="">Neli Kalistova</a>
-        <a className='tab3 general-tab' href=''>Proceso</a>
-        <a className='tab4 general-tab' href=''>Contacto</a>
+      <header id="header-id">
+        <a className="tab1 general-tab" href="">
+          Pinturas
+        </a>
+        <a className="tab2 general-tab" href="">
+          Murales
+        </a>
+        <a className="logo general-tab" href="">
+          Neli Kalistova
+        </a>
+        <a className="tab3 general-tab" href="">
+          Proceso
+        </a>
+        <a className="tab4 general-tab" href="">
+          Contacto
+        </a>
       </header>
       <main>
-        <section className='hero-section'>
-          <div className='title-couple'>
-            <h1>Mur<span className='spacing5'></span>a<span className='spacing1'></span>les</h1>
-            <img className='image-hero' src={tigerPicture} alt="tiger" />
-            <h1 className='italic'>a medida,</h1>
+        {/* Hero Section */}
+        <section className="hero-section">
+          <div className="title-couple">
+            <h1>
+              Mur<span className="spacing5"></span>a<span className="spacing1"></span>les
+            </h1>
+            {/* <div className="image-hero-container"></div> */}
+            <img className="image-hero" src={tigerPicture} alt="tiger" />
+            <h1 className="italic">a medida,</h1>
           </div>
-          <h1 className='title-bottom'><span className='italic'>pintura de</span> caballete</h1>
-          <p className='hero-paragraph'>El primer paso hacia tu obra única</p>
-          <button className='general-button' id='hero-button'>Iniciar mi proyecto</button>
+          <h1 className="title-bottom">
+            <span className="italic">pintura de</span> caballete
+          </h1>
+          <p className="hero-paragraph">El primer paso hacia tu obra única</p>
+          <button className="general-button" id="hero-button">
+            Iniciar mi proyecto
+          </button>
         </section>
-        <section className='intro-section'>
-          <img className='image-intro' src={tigerPicture} alt='tiger' />
-          <p className='paragraph-intro serif'>
+
+        {/* Intro Section */}
+        <section className="intro-section">
+          <img className="image-intro" src={tigerPicture} alt="tiger" style={{ opacity: "0.1" }} />
+          <p className="paragraph-intro serif">
             Conectando el carácter del espacio con una narrativa pictórica personal
           </p>
         </section>
-        <section className='about-section'>
+
+        {/* About Section */}
+        <section className="about-section">
           <div className="about-title-container">
-            <h2 className='about-title'>Sobre <span className='italic'>mí</span></h2>
+            <img src={sobreMi} alt="" style={{ width: "100%" }} />
           </div>
 
           <img src="" alt="" />
-          <p className='serif'>Formación Superior y Experiencia Profesional</p>
+          <p className="serif">Formación Superior y Experiencia Profesional</p>
           <p style={{ textAlign: "center" }}>
             Estudié en la Universidad Estatal Stroganov de Moscú, de Arte y Diseño Industrial, con
             <br></br>
@@ -45,17 +65,18 @@ function App() {
             <br></br>
             Trabajé como artista y diseñadora en una empresa de tejidos para interiores.
             <br></br>
-            Participé en múltiples exposiciones internacionales.</p>
+            Participé en múltiples exposiciones internacionales.
+          </p>
         </section>
-        <section className='pinturas'>
-          <h2 className='pinturas-title'>Pinturas</h2>
+
+        {/* Paintings Section */}
+        <section className="pinturas">
+          <h2 className="pinturas-title">Pinturas</h2>
         </section>
       </main>
-      <footer>
-
-      </footer>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
